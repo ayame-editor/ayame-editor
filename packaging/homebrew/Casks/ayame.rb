4031743c5df1bad53159b4d00f9d5ae94d1973ec
hjosugi <h.jo.sugi@gmail.com>
2026-07-12 21:30:38 +0900

    chore(packaging): update manifests for v0.7.0

--- packaging/homebrew/Casks/ayame.rb
+++ packaging/homebrew/Casks/ayame.rb
@@ -1,9 +1,9 @@
 cask "ayame" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.6.1"
-  sha256 arm:   "6d3181f32ad699e0ab12087e156727bf5ae9110e13d4688d83869b8f170d763a",
-         intel: "efb2466725e210f97d47bcdd6cc616ba9d81115a4b3d53ddc9548a700972ed40"
+  version "0.7.0"
+  sha256 arm:   "9cb9144ae1177289b1843240c226fdfcb4f01df874dd4705734f7ed0976a5c59",
+         intel: "bc2d993888f1aaffe71765b38d91db4f029ee421e46179fe4a4c1b43681200b5"
 
   url "https://github.com/hjosugi/ayame-editor/releases/download/v#{version}/ayame-v#{version}-macos-#{arch}.zip"
   name "Ayame Editor"
--- packaging/homebrew/Formula/ayame.rb
+++ packaging/homebrew/Formula/ayame.rb
@@ -1,18 +1,18 @@
 class Ayame < Formula
   desc "Desktop text editor and CLI tools for huge files"
   homepage "https://github.com/hjosugi/ayame-editor"
-  version "0.6.1"
+  version "0.7.0"
   license "0BSD"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/hjosugi/ayame-editor/releases/download/v0.6.1/ayame-v0.6.1-macos-aarch64.zip"
-    sha256 "6d3181f32ad699e0ab12087e156727bf5ae9110e13d4688d83869b8f170d763a"
+    url "https://github.com/hjosugi/ayame-editor/releases/download/v0.7.0/ayame-v0.7.0-macos-aarch64.zip"
+    sha256 "9cb9144ae1177289b1843240c226fdfcb4f01df874dd4705734f7ed0976a5c59"
   elsif OS.mac?
-    url "https://github.com/hjosugi/ayame-editor/releases/download/v0.6.1/ayame-v0.6.1-macos-x86_64.zip"
-    sha256 "efb2466725e210f97d47bcdd6cc616ba9d81115a4b3d53ddc9548a700972ed40"
+    url "https://github.com/hjosugi/ayame-editor/releases/download/v0.7.0/ayame-v0.7.0-macos-x86_64.zip"
+    sha256 "bc2d993888f1aaffe71765b38d91db4f029ee421e46179fe4a4c1b43681200b5"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/hjosugi/ayame-editor/releases/download/v0.6.1/ayame-v0.6.1-linux-x86_64"
-    sha256 "0acad457d6ded97c77dde38abfffcd1b61b07ffc1a8b475e5bf04be36731d180"
+    url "https://github.com/hjosugi/ayame-editor/releases/download/v0.7.0/ayame-v0.7.0-linux-x86_64"
+    sha256 "3a7ee399aef84899aa88bfe0fdd1babd7379810f4d8ab0dd9233a50013a25c9f"
   else
     odie "Ayame prebuilt Homebrew formula currently supports macOS and Linux x86_64"
   end
